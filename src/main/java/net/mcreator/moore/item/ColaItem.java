4a123debe362fdb0fff4fd1dc5ed9ece8282efae
Altarplayer <heitz@192.168.1.62>
2021-01-09 18:48:55 +0100

    Cola opérationnel

--- src/main/java/net/mcreator/moore/item/ColaItem.java
+++ src/main/java/net/mcreator/moore/item/ColaItem.java
@@ -3,14 +3,20 @@ package net.mcreator.moore.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
+import net.minecraft.world.World;
 import net.minecraft.item.UseAction;
 import net.minecraft.item.Rarity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.Food;
+import net.minecraft.entity.LivingEntity;
 
+import net.mcreator.moore.procedures.ColaFoodEatenProcedure;
 import net.mcreator.moore.itemgroup.MooreItemGroup;
 import net.mcreator.moore.MooreModElements;
+
+import java.util.Map;
+import java.util.HashMap;
 
 @MooreModElements.ModElement.Tag
 public class ColaItem extends MooreModElements.ModElement {
@@ -27,8 +33,13 @@ public class ColaItem extends MooreModElements.ModElement {
 	public static class FoodItemCustom extends Item {
 		public FoodItemCustom() {
 			super(new Item.Properties().group(MooreItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON)
-					.food((new Food.Builder()).hunger(2).saturation(0.5f).build()));
+					.food((new Food.Builder()).hunger(1).saturation(0.5f).setAlwaysEdible().build()));
 			setRegistryName("cola");
+		}
+
+		@Override
+		public int getUseDuration(ItemStack stack) {
+			return 5;
 		}
 
 		@Override
@@ -39,6 +50,20 @@ public class ColaItem extends MooreModElements.ModElement {
 		@Override
 		public net.minecraft.util.SoundEvent getEatSound() {
 			return net.minecraft.util.SoundEvents.ENTITY_GENERIC_DRINK;
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
+			ItemStack retval = super.onItemUseFinish(itemstack, world, entity);
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("entity", entity);
+				ColaFoodEatenProcedure.executeProcedure($_dependencies);
+			}
+			return retval;
 		}
 	}
 }
---
+++ src/main/java/net/mcreator/moore/procedures/ColaFoodEatenProcedure.java
@@ -0,0 +1,28 @@
+package net.mcreator.moore.procedures;
+
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.moore.MooreModElements;
+
+import java.util.Map;
+
+@MooreModElements.ModElement.Tag
+public class ColaFoodEatenProcedure extends MooreModElements.ModElement {
+	public ColaFoodEatenProcedure(MooreModElements instance) {
+		super(instance, 18);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure ColaFoodEaten!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SPEED, (int) 60, (int) 1));
+	}
+}
